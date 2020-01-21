24bc46502264141ea47d2efdf484b82036dc294c
anshul.g <anshul.g@Mac-Pro.local>
2020-01-22 01:50:22 +0530

    Added Sender app. Cross-platform (Telegram to Flock) only-text messaging working

--- src/main/java/org/example/Mediator_app.java
+++ src/main/java/org/example/Mediator_app.java
@@ -4,14 +4,20 @@ public class Mediator_app implements Sender {
 
     Receiver_app recv_app;
     Sender_app send_app;
+
+
     @Override
     public void send(String msg) {
         send_app.send(msg);
     }
 
-    public static void main(){
+    Mediator_app(){
         Receiver_app recv_app = new Receiver_app(this);
         Sender_app send_app = new Sender_app();
+    }
+
+    public static void main(){
+        new Mediator_app();
     }
 
 }
